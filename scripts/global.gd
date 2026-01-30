9897d448ed8e8177463fb8ff006722d2ea865f66
Olaf Łogin <me@olaaaf.com>
2026-01-30 22:25:41 +0100

    add singleton

---
+++ scripts/global.gd
@@ -0,0 +1,13 @@
+class_name GlobalSingleton extends Node
+
+static var global: GlobalSingleton = null
+
+# Use this singleton in any scene with:
+# Global.(fuction or field defined in this file)s
+
+func _init() -> void:
+	if global == null:
+		global = self
+	else:
+		printerr("Trying to create another instance of MySingleton. Deleting it.")
+		queue_free()
