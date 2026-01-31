3e49e973a04a7d82fff012b5ed98ce56bf51268f
Olaf Łogin <me@olaaaf.com>
2026-01-31 04:43:11 +0100

    background (#25)

--- scripts/background.gd
+++ scripts/background.gd
@@ -1,16 +1,21 @@
 extends Node2D
 
-@export var player: Player
-@export_range(0.1,100, 0.1) var speed_multiplier :float = 1
+@export var player: RigidBody2D
+@export_range(0.01,0.05, 0.001) var speed_multiplier :float = 1
 var proportions = []
 var children : Array[Node]
 
-func _init() -> void:
+func _ready() -> void:
 	children = get_children()
+	print(children)
 	for child in children:
 		proportions.append((child as Parallax2D).autoscroll.x)
 
-func _ready() -> void:
-	var v_x = player.velocity.x
+func _process(delta) -> void:
+	var v_x = player.linear_velocity.x as float
+	global_rotation_degrees = 0
+	var i = 0
 	for child in children:
-		(child as Parallax2D).autoscroll.x = proportions * v_x * global_scale
+		(child as Parallax2D).autoscroll = Vector2((proportions[i] * v_x * speed_multiplier) as float, 0)
+		print("autoscroll.x: %f", (child as Parallax2D).autoscroll.x)
+		i += 1
