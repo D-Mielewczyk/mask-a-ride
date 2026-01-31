d67e835914460abe8db6af9b928b126497cad24d
Olaf Łogin <me@olaaaf.com>
2026-01-31 04:39:29 +0100

    background

--- scenes/camera_2d.gd
+++ scenes/camera_2d.gd
@@ -5,6 +5,11 @@ extends Camera2D
 @export var min_zoom = 0.5
 @export var max_zoom = 0.25
 @export var speed_threshold = 2000.0
+@export var bg: Node2D
+var bg_initial_scale = 0
+
+func _ready() -> void:
+	bg_initial_scale = bg.scale.x
 
 func _physics_process(delta):
 	# Jeśli nie przypisaliśmy noda w inspektorze, szukamy rodzica
@@ -17,4 +22,6 @@ func _physics_process(delta):
 		
 		# Wolny lerp (np. 1.5) = filmowe, płynne oddalanie
 		# Szybki lerp (np. 5.0) = dynamiczne, agresywne oddalanie
-		zoom = zoom.lerp(Vector2(target_z, target_z), delta * 2.0)
+		#zoom = zoom.lerp(Vector2(target_z, target_z), delta * 2.0)
+		#if bg != null:
+		#	bg.scale = Vector2(bg_initial_scale / scale.x, bg_initial_scale / scale.y)
