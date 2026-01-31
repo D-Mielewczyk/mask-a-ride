9584279abce66b4485a58408cbaad6c82d6e95c1
dawid.mielewczyk@naviparking.com <55713797+D-Mielewczyk@users.noreply.github.com>
2026-01-31 18:42:55 +0100

    hotfix tileset terrain

--- scripts/chasm_chunk.gd
+++ scripts/chasm_chunk.gd
@@ -13,7 +13,7 @@ extends Node2D
 @export_group("Random Drop Settings")
 @export var min_drop_height: float = 200.0 # Minimalny uskok w dół
 @export var max_drop_height: float = 700.0 # Maksymalny uskok (głęboki zjazd)
-@export var tile_uv_scale: float = 0.08
+@export var tile_uv_scale: float = 0.2
 
 # Punkt końcowy dla Managera Świata
 var end_point_world: Vector2
